88b3bdd9e3a953a1e1cfb95d6b8a60d68699425a
Daniel-Wonders <lautaroalloco@gmail.com>
2025-06-23 15:59:58 -0300

    por el 5

--- ej.pl
+++ ej.pl
@@ -58,3 +58,45 @@ menorOIgual(X, X) :-
 
 menorOIgual(X, suc(Y)) :- 
     menorOIgual(X, Y).
+
+%--------------- 4 -------------
+/*enir el predicado juntar(?Lista1,?Lista2,?Lista3), que tiene éxito si Lista3 es la concatenación de
+Lista1 y Lista2. Por ejemplo:
+?- juntar([a,b,c], [d,e], [a,b,c,d,e]). → true.
+?- juntar([a,b,c], [d,e], L). → L = [a,b,c,d,e].
+?- juntar([a,b,c], L, [a,b,c,d,e]). → L = [d,e].
+?- juntar(L, [d,e], [a,b,c,d,e]). → L = [a,b,c].
+?- juntar(L1, L2, [1,2,3]). → L1 = [], L2 = [1, 2, 3]; L1 = [1], L2 = [2, 3];
+L1 = [1,2], L2 = [3]; L1 = [1,2,3], L2 = [].
+Al igual que la mayoría de los predicados, puede dar false después de agotar los resultados.*/
+
+% Caso base: concatenar lista vacía con L2 da como resultado L2
+juntar([], L2, L2).
+
+% Paso recursivo: agregar cabeza de L1 al resultado de concatenar el resto
+juntar([X|XS], L2, [X|ZS]) :-
+    juntar(XS, L2, ZS).
+
+%---------------- 5 -----------------
+/*Denir los siguientes predicados sobre listas usando append:
+i. last(?L, ?U), donde U es el último elemento de la lista L.
+ii. reverse(+L, ?R), donde R contiene los mismos elementos que L, pero en orden inverso.
+Ejemplo: reverse([a,b,c], [c,b,a]).
+Mostrar el árbol de búsqueda para el ejemplo dado.
+iii. prefijo(?P, +L), donde P es prejo de la lista L.
+iv. sufijo(?S, +L), donde S es sujo de la lista L.
+v. sublista(?S, +L), donde S es sublista de L.
+vi. pertenece(?X, +L), que es verdadero sii el elemento X se encuentra en la lista L. (Este predicado ya viene
+denido en Prolog y se llama member).
+*/
+
+%i
+%!last(?L,?U)
+/*last(Lista,Ultimo):-
+    length(Lista,Largo),
+    Xlargo is Largo -1,
+    length(Xlista,Xlargo),
+    append(Xlista,[Ultimo],Lista).*/
+last(Lista,Ultimo):-append(_,[Ultimo],Lista).
+
+reverse(L,R):-
