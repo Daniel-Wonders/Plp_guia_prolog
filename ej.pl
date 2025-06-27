dbae7e05088176284f87836e615a617d2e872998
Lautaro <lautaroalloco@gmail.com>
2025-06-26 22:28:50 -0300

    23???

--- ej.pl
+++ ej.pl
@@ -563,3 +563,43 @@ sumatoria2([], 0).
 sumatoria2([X|XS], S) :-
     sumatoria2(XS, SRest),
     S is X + SRest.
+
+%------------- 23 ----------
+
+/*Trabajaremos con árboles binarios, usando nil y bin(AI, V, AD) para representarlos en Prolog.
+i. Implementar un predicado arbol(-A) que genere estructuras de árbol binario, dejando los valores de los nodos sin instanciar.
+Deben devolverse todos los árboles posibles (es decir, para toda
+estructura posible, el predicado debe devolverla luego de un número nito de pedidos). No debe devolverse dos veces el mismo árbol.
+? arbol(A).
+A = nil ;
+A = bin(nil, _G2388, nil) ;
+A = bin(nil, _G2391, bin(nil, _G2398, nil)) ;
+A = bin(bin(nil, _G2398, nil), _G2391, nil) ;*/
+arbol(nil).
+arbol(bin(I,_,D)):-
+    arbol(I),
+    arbol(D).
+/*ii. Implementar un predicado nodosEn(?A, +L) que es verdadero
+cuando A es un árbol cuyos nodos pertenecen al conjunto conjunto
+de átomos L (representado mediante una lista no vacía, sin orden
+relevante y sin repetidos). Puede asumirse que el árbol se recibe
+instanciado en su estructura, pero no necesariamente en sus nodos.
+? arbol(A), nodosEn(A, [ka, pow]).
+A = nil ;
+A = bin(nil, ka, nil) ;
+A = bin(nil, pow, nil) ;
+A = bin(nil, ka, bin(nil, ka, nil)) ;
+A = bin(nil, ka, bin(nil, pow, nil)) ;*/
+nodosEn()
+
+/*iii. Implementar un predicado sinRepEn(-A, +L) que genere todos los árboles cuyos nodos pertenezcan al alfabeto L y usando como máximo una
+vez cada símbolo del mismo. En este caso, no hay innitos árboles posibles; es importante que el predicado no devuelva soluciones repetidas y
+que no se quede buscando indenidamente una vez terminado el espacio
+de soluciones.
+? arbolSinRepEn(A, [ka, pow]).
+A = nil ;
+A = bin(nil, ka, nil) ; ...
+A = bin(nil, ka, bin(nil, pow, nil)) ;
+... ;
+No.*/
+
